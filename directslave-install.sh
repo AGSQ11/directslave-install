704576d6dbe2e63736437dd6a417f030b59a5aa8
AGSQ <67051233+AGSQ11@users.noreply.github.com>
2024-12-20 19:52:59 +0200

    Update directslave-install.sh

--- directslave-install.sh
+++ directslave-install.sh
@@ -2,64 +2,48 @@
 # @author jordavin,phillcoxon,mantas15
 # @updated by Afrizal-id
 # @updated later by osmanboy / ericosman @forum.directadmin.com
-# @date 27.01.2024
-# @version 1.0.6
-# @source 
-# ------------------------------------------------------------------------------
+# @updated for Ubuntu by AGSQ
+# @date 20.12.2024
+# @version 1.0.7
+
 sshport=22;
-#Check that user is root.
-if [ “$(id -u)” = “0” ]; then
-printf "Bingo! you are root. Continue on....\n"
-  else
-printf "Sorry, This script must be run as root\n"
-exit;
+
+# Check root user
+if [ "$(id -u)" != "0" ]; then
+    printf "Sorry, This script must be run as root\n"
+    exit 1
 fi
-#What Distro are you on? --> doenst work, have to look into it
-#printf "Distro are you on??\n" 2>&1
-#OS='cat /etc/redhat-release | awk {'print $1}'
-#if [ "$OS" = "CentOS" ]; then
-#echo "System runs on CentOS 7.X. Checking Continue on....";
-#VN='cat /etc/redhat-release | awk {'print $3}'
-#else [ "$VN" != "7.*" ]; elseif
-#echo "Installation failed. System runs on unsupported Linux. Exiting...";
-#exit;
-#fi 
-if [ -z "$1" ]; then
- echo "usage <username> <userpass> <master ip>";
- exit 0;
+
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    echo "usage <username> <userpass> <master ip>";
+    exit 0;
 fi
-if [ -z "$2" ]; then
- echo "usage <username> <userpass> <master ip>";
- exit 0;
-fi
-if [ -z "$3" ]; then
- echo "usage <username> <userpass> <master ip>";
- exit 0;
-fi
+
 echo "Saving most outputs to /root/install.log";
 
 echo "doing updates and installs"
-yum update -y > /root/install.log
-yum install epel-release -y >> /root/install.log
-yum install bind bind-utils wget -y >> /root/install.log
+apt-get update -y > /root/install.log
+apt-get upgrade -y >> /root/install.log
+apt-get install bind9 bind9utils wget -y >> /root/install.log
 
-systemctl start named >> /root/install.log
-systemctl stop named >> /root/install.log
+systemctl start bind9 >> /root/install.log
+systemctl stop bind9 >> /root/install.log
 
-echo "creating user "$1" and adding to wheel"
-useradd -G wheel $1 > /root/install.log
-echo $2 |passwd $1 --stdin  >> /root/install.log
+echo "creating user "$1" and adding to sudo group"
+useradd -m -G sudo $1 > /root/install.log
+echo "$1:$2" | chpasswd >> /root/install.log
+
 echo "Disabling root access to ssh use "$1"."
-echo -n "${MAGENTA}Enter SSH port to change (recommended) from ${BLUE}${sshport}${MAGENTA}:${NORMAL} "
-		read customsshport
-		if [ $customsshport ]; then
-			sshport=$customsshport
-		fi
+echo -n "Enter SSH port to change (recommended) from ${sshport}: "
+read customsshport
+if [ $customsshport ]; then
+    sshport=$customsshport
+fi
 echo "Your ssh port is ${sshport}"
 sed -i '/PermitRootLogin/ c\PermitRootLogin no' /etc/ssh/sshd_config
 sed -i -e "s/#Port 22/Port ${sshport}/g" /etc/ssh/sshd_config
 sed -i -e 's/#UseDNS yes/UseDNS no/g' /etc/ssh/sshd_config
-systemctl restart sshd  >> /root/install.log
+systemctl restart ssh >> /root/install.log
 
 echo "installing and configuring directslave"
 cd ~
@@ -69,7 +53,7 @@ mv directslave /usr/local/
 cd /usr/local/directslave/bin
 mv directslave-linux-amd64 directslave
 cd /usr/local/directslave/
-chown named:named -R /usr/local/directslave
+chown bind:bind -R /usr/local/directslave
 
 curip="$( hostname -I|awk '{print $1}' )"
 cat > /usr/local/directslave/etc/directslave.conf <<EOF
@@ -80,101 +64,63 @@ ssl             off
 cookie_sess_id  DS_SESSID
 cookie_auth_key Change_this_line_to_something_long_&_secure
 debug           0
-uid             25
-gid             25
+uid             102
+gid             102
 pid             /usr/local/directslave/run/directslave.pid
 access_log	/usr/local/directslave/log/access.log
 error_log	/usr/local/directslave/log/error.log
 action_log	/usr/local/directslave/log/action.log
-named_workdir   /etc/namedb/secondary
-named_conf	/etc/namedb/directslave.inc
+named_workdir   /etc/bind/secondary
+named_conf	/etc/bind/directslave.inc
 retry_time	1200
 rndc_path	/usr/sbin/rndc
 named_format    text
 authfile        /usr/local/directslave/etc/passwd
 EOF
 
-#mkdir /etc/namedb
-mkdir -p /etc/namedb/secondary
-touch /etc/namedb/secondary/named.conf
-touch /etc/namedb/directslave.inc
-chown named:named -R /etc/namedb
-mkdir /var/log/named
+mkdir -p /etc/bind/secondary
+touch /etc/bind/secondary/named.conf
+touch /etc/bind/directslave.inc
+chown bind:bind -R /etc/bind
+mkdir -p /var/log/named
 touch /var/log/named/security.log
 chmod a+w -R /var/log/named
 
-cat > /etc/named.conf <<EOF
-//
-// named.conf
-//
-// Provided by Red Hat bind package to configure the ISC BIND named(8) DNS
-// server as a caching only nameserver (as a localhost DNS resolver only).
-//
-// See /usr/share/doc/bind*/sample/ for example named configuration files.
-//
-// See the BIND Administrator's Reference Manual (ARM) for details about the
-// configuration located in /usr/share/doc/bind-{version}/Bv9ARM.html
+cat > /etc/bind/named.conf.options <<EOF
 options {
+	directory "/var/cache/bind";
 	listen-on port 53 { any; };
-	listen-on-v6 port 53 { none; };
-	directory 	"/var/named";
-	dump-file 	"/var/named/data/cache_dump.db";
-	statistics-file "/var/named/data/named_stats.txt";
-	memstatistics-file "/var/named/data/named_mem_stats.txt";
-	recursing-file  "/var/named/data/named.recursing";
-	secroots-file   "/var/named/data/named.secroots";
-		allow-query     { any; };
-		allow-notify	{ $3; };
-		allow-transfer	{ $3; };
-	/*
-	 - If you are building an AUTHORITATIVE DNS server, do NOT enable recursion.
-	 - If you are building a RECURSIVE (caching) DNS server, you need to enable
-	   recursion.
-	 - If your recursive DNS server has a public IP address, you MUST enable access
-	   control to limit queries to your legitimate users. Failing to do so will
-	   cause your server to become part of large scale DNS amplification
-	   attacks. Implementing BCP38 within your network would greatly
-	   reduce such attack surface
-	*/
+	listen-on-v6 { none; };
+        allow-query     { any; };
+        allow-notify	{ $3; };
+        allow-transfer	{ $3; };
 	recursion no;
-	dnssec-enable yes;
-	dnssec-validation yes;
-	/* Path to ISC DLV key */
-	bindkeys-file "/etc/named.iscdlv.key";
-	managed-keys-directory "/var/named/dynamic";
-	pid-file "/run/named/named.pid";
-	session-keyfile "/run/named/session.key";
+	dnssec-validation auto;
+	auth-nxdomain no;
 };
-logging {
-        channel default_debug {
-                file "data/named.run";
-                severity dynamic;
-        };
-};
-zone "." IN {
-	type hint;
-	file "named.ca";
-};
-include "/etc/named.rfc1912.zones";
-include "/etc/named.root.key";
-include "/etc/namedb/directslave.inc";
+EOF
+
+cat > /etc/bind/named.conf.local <<EOF
+include "/etc/bind/directslave.inc";
 EOF
 
 touch /usr/local/directslave/etc/passwd
-chown named:named /usr/local/directslave/etc/passwd
+chown bind:bind /usr/local/directslave/etc/passwd
 /usr/local/directslave/bin/directslave --password $1:$2
-/usr/local/directslave/bin/directslave --check  >> /root/install.log
-rm /usr/local/directslave/run/directslave.pid
+/usr/local/directslave/bin/directslave --check >> /root/install.log
+rm -f /usr/local/directslave/run/directslave.pid
 
 cat > /etc/systemd/system/directslave.service <<EOL
 [Unit]
 Description=DirectSlave for DirectAdmin
 After=network.target
+
 [Service]
 Type=simple
-User=named
+User=bind
 ExecStart=/usr/local/directslave/bin/directslave --run
 Restart=always
+
 [Install]
 WantedBy=multi-user.target
 EOL
@@ -183,22 +129,19 @@ echo "setting enabled and starting up"
 chown root:root /etc/systemd/system/directslave.service
 chmod 755 /etc/systemd/system/directslave.service
 systemctl daemon-reload >> /root/install.log
-systemctl enable named >> /root/install.log
+systemctl enable bind9 >> /root/install.log
 systemctl enable directslave >> /root/install.log
-systemctl restart named >> /root/install.log
+systemctl restart bind9 >> /root/install.log
 systemctl restart directslave >> /root/install.log
-echo "adding simple firewalld and opening Firewalld ports"
-yum update -y > /root/install.log
-yum install firewalld -y >> /root/install.log
 
-systemctl start firewalld >> /root/install.log
-systemctl enable firewalld >> /root/install.log
-firewall-cmd --permanent --add-service=dns
-firewall-cmd --permanent --add-port=2222/tcp
-firewall-cmd --permanent --add-port=53/tcp
-firewall-cmd --permanent --add-port=443/tcp
-firewall-cmd --reload
-systemctl start firewalld >> /root/install.log
+echo "configuring UFW firewall"
+apt-get install ufw -y >> /root/install.log
+ufw allow 53/tcp
+ufw allow 53/udp
+ufw allow 2222/tcp
+ufw allow $sshport/tcp
+ufw allow 443/tcp
+ufw --force enable
 
 echo "Checking DirectSlave and starting"
 /usr/local/directslave/bin/directslave --check
